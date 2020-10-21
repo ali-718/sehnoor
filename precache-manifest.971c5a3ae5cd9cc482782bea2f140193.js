0ae92a9cfd42ccd0f896980d38efe9673ab04234
Ali Haider <alihaider@Alis-MacBook-Pro.local>
2020-10-21 15:51:21 +0500

    images changed

---
+++ precache-manifest.971c5a3ae5cd9cc482782bea2f140193.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d94c2f2a85ba9edee5a71ed3d9eee108",
+    "url": "/sehnoor/index.html"
+  },
+  {
+    "revision": "5df4fb035c44e9a75b4a",
+    "url": "/sehnoor/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "45990859fbfe97ec75ca",
+    "url": "/sehnoor/static/css/main.6e39dcf5.chunk.css"
+  },
+  {
+    "revision": "5df4fb035c44e9a75b4a",
+    "url": "/sehnoor/static/js/2.e49e5668.chunk.js"
+  },
+  {
+    "revision": "0d6c4affe2a659999d44765f9a17cb4f",
+    "url": "/sehnoor/static/js/2.e49e5668.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "45990859fbfe97ec75ca",
+    "url": "/sehnoor/static/js/main.e4b1c17d.chunk.js"
+  },
+  {
+    "revision": "ae82176b0639cdaefe5e",
+    "url": "/sehnoor/static/js/runtime-main.f5692e91.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/sehnoor/static/media/logo.5d5d9eef.svg"
+  }
+]);
